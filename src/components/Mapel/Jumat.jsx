9b55fb18967bd374b93053b8c77e6d4fd2c77820
DAFFA EVAN <daffaevan673@gmail.com>
2024-02-02 20:54:39 +0700

    pesan commit

--- src/components/Mapel/Jumat.jsx
+++ src/components/Mapel/Jumat.jsx
@@ -5,7 +5,7 @@ const Jumat = () => {
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">TKJ</div>
+				<div className="w-[50%] text-base  font-medium">Bimbingan Konseling</div>
 				<div className="flex justify-center items-center text-sm">07.20-08.00</div>
 			</div>
 			<div
@@ -13,14 +13,14 @@ const Jumat = () => {
 				data-aos="fade-up"
 				data-aos-duration="600">
 				<div className="w-[50%] text-base  font-medium">Agama</div>
-				<div className="flex justify-center items-center text-sm">08.00-09.20</div>
+				<div className="flex justify-center items-center text-sm">08.00-09.50</div>
 			</div>
 			<div
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="700">
 				<div className="w-[50%] text-base  font-medium">Bahasa Inggris</div>
-				<div className="flex justify-center items-center text-sm">09.40-11.30</div>
+				<div className="flex justify-center items-center text-sm">10.10-11.30</div>
 			</div>
 		</>
 	)
